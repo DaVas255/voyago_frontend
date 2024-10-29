e9c826cc1982dc175c10536bbfd9043171dd0139
Danil Vasilchikov <vda@2i.tusur.ru>
2024-10-30 00:19:17 +0700

    add order service

---
+++ src/service/order.service.ts
@@ -0,0 +1,41 @@
+import { API_URL } from "@/app/constants";
+import { IOrder } from "@/app/types/types";
+import { getAccessToken } from "./auth/auth.helper";
+
+export async function getOrders() {
+  try {
+    const response = await fetch(API_URL + `/orders`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function createOrder(data: IOrder) {
+  try {
+    const response = await fetch(API_URL + `/orders`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) throw new Error(response.statusText);
+
+    console.log(await response.json());
+  } catch (error) {
+    console.error(error);
+  }
+}
