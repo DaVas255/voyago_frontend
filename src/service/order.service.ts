05737bd75518a1b01fd1259dd2828527acf6feae
Danil Vasilchikov <DaVas255@gmail.com>
2024-11-05 20:08:35 +0700

    requests for orders

--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -39,3 +39,39 @@ export async function createOrder(data: IOrder) {
     console.error(error);
   }
 }
+
+export async function getOrdersByUser() {
+  try {
+    const response = await fetch(API_URL + `/orders/user`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function updateOrder(orderId: number) {
+  try {
+    const response = await fetch(API_URL + `/orders/${orderId}/complete`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
