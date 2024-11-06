41df379a3b3ebadb262a912d05a491f9db80188f
Danil Vasilchikov <DaVas255@gmail.com>
2024-11-06 18:11:12 +0700

    update create order form

--- src/components/features/CreateOrderForm/CreateOrderForm.tsx
+++ src/components/features/CreateOrderForm/CreateOrderForm.tsx
@@ -7,6 +7,8 @@ import styles from './CreateOrderForm.module.scss'
 import { IOrder } from '@/app/types/types'
 import { createOrder } from '@/service/order.service'
 import { Input } from '@/components/ui/Input/Input'
+import { TextArea } from '@/components/ui/textarea/TextArea'
+import { Button } from '@/components/ui/button/Button'
 
 export const CreateOrderForm = () => {
   const navigate = useNavigate()
@@ -34,17 +36,27 @@ export const CreateOrderForm = () => {
     <form onSubmit={handleSubmit(onSubmit)} className={styles['create-order']}>
       <h1 className={styles['create-order__title']}>Создать заказ</h1>
       <Input type='text' placeholder='Название' {...register('title', { required: true })} />
-      <Input type='text' placeholder='Описание' {...register('description', { required: true })} />
+      <TextArea placeholder='Описание' {...register('description', { required: true })} />
       <Input type='text' placeholder='Локация' {...register('location', { required: true })} />
-      <Input type='date' placeholder='Дата начала' {...register('startDate', { required: true })} />
-      <Input
-        type='date'
-        placeholder='Дата окончания'
-        {...register('endDate', { required: true })}
+      <div className={styles['create-order__dates']}>
+        <Input
+          type='date'
+          placeholder='Дата начала'
+          {...register('startDate', { required: true })}
+        />
+        <Input
+          type='date'
+          placeholder='Дата окончания'
+          {...register('endDate', { required: true })}
+        />
+      </div>
+
+      <Button
+        type='submit'
+        className={styles['create-order__submit']}
+        name='Создать'
+        disabled={isOrderPending}
       />
-      <button type='submit' className={styles['create-order__submit']} disabled={isOrderPending}>
-        Создать
-      </button>
     </form>
   )
 }
