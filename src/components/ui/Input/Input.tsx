f2050f43dbd3a9749267b5761dad49fda1260a15
Danil Vasilchikov <DaVas255@gmail.com>
2024-11-08 04:03:28 +0700

    update ui input

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,5 +1,7 @@
 import { forwardRef } from 'react'
 import styles from './Input.module.scss'
+import clsx from 'clsx'
+import { FieldError } from 'react-hook-form'
 
 interface InputProps {
   type: string
@@ -7,13 +9,25 @@ interface InputProps {
   value?: string
   defaultValue?: any
   disabled?: boolean
+  error?: FieldError
+  errorText?: string
   className?: string
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ type, placeholder, ...props }, ref) => {
+  ({ type, placeholder, error, errorText, disabled, ...props }, ref) => {
     return (
-      <input type={type} placeholder={placeholder} className={styles.input} ref={ref} {...props} />
+      <div className={styles['input-container']}>
+        <input
+          type={type}
+          placeholder={placeholder}
+          className={clsx(styles.input, error && styles['input-error'])}
+          disabled={disabled}
+          ref={ref}
+          {...props}
+        />
+        {error && <label className={styles['input__error-label']}>{errorText}</label>}
+      </div>
     )
   },
 )
