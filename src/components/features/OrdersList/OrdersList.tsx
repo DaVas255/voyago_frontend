1dca3a0b8c5086280f8c90265f564059b378db5d
Danil Vasilchikov <DaVas255@gmail.com>
2024-11-09 00:18:53 +0700

    redesign orders list page

--- src/components/features/OrdersList/OrdersList.tsx
+++ src/components/features/OrdersList/OrdersList.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import clsx from 'clsx'
 
 import styles from './OrdersList.module.scss'
 import { Loader } from '@/components/ui/Loader/Loader'
@@ -38,16 +39,30 @@ export const OrdersList = () => {
         type='button'
         name='Создать новый заказ'
         className={styles.orders__create}
+        background
         onClick={() => navigate('/orders/new')}
       />
       <div className={styles.orders__list}>
         {orders.length > 0 ? (
           orders.map(order => (
             <div key={order.id} className={styles.orders__item}>
-              <div>{order.title}</div>
-              <div>{order.description}</div>
-              <div>{order.location}</div>
-              <div>{order.user.name}</div>
+              <div className={styles.orders__title}>{order.title}</div>
+              <div className={styles.orders__name}>
+                <span className={styles.orders__label}>Описание: </span>
+                {order.description}
+              </div>
+              <div className={styles.orders__name}>
+                <span className={styles.orders__label}>Локация: </span>
+                {order.location}
+              </div>
+              <div className={clsx(styles.orders__name, styles.orders__author)}>
+                <span className={styles.orders__label}>Автор: </span>
+                {order.user.name}
+              </div>
+              <div className={styles.orders__name}>
+                <span className={styles.orders__label}>Дата: </span>
+                {order.startDate.split('T')[0]} - {order.endDate.split('T')[0]}
+              </div>
             </div>
           ))
         ) : (
