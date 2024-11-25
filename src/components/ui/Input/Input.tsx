09e38cfe422e6b957b1ebe58139156f63403c57a
Danil Vasilchikov <DaVas255@gmail.com>
2024-11-25 21:55:41 +0700

    feat: eye for password input

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,7 +1,10 @@
-import { forwardRef } from 'react'
-import styles from './Input.module.scss'
+import { forwardRef, useState } from 'react'
 import clsx from 'clsx'
 import { FieldError } from 'react-hook-form'
+
+import styles from './Input.module.scss'
+import Eye from '@/app/assets/icons/eye.svg?react'
+import EyeSlash from '@/app/assets/icons/eye-slash.svg?react'
 
 interface InputProps {
   type: string
@@ -16,16 +19,32 @@ interface InputProps {
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ type, placeholder, error, errorText, disabled, ...props }, ref) => {
+    const [isPasswordVisible, setPasswordVisible] = useState(false)
+
+    const togglePasswordVisibility = () => {
+      setPasswordVisible(!isPasswordVisible)
+    }
+
     return (
       <div className={styles['input-container']}>
         <input
-          type={type}
+          type={type === 'password' && isPasswordVisible ? 'text' : type}
           placeholder={placeholder}
           className={clsx(styles.input, error && styles['input-error'])}
           disabled={disabled}
           ref={ref}
           {...props}
         />
+        {type === 'password' && (
+          <button
+            type='button'
+            className={styles['toggle-password']}
+            onClick={togglePasswordVisibility}
+            aria-label={isPasswordVisible ? 'Скрыть пароль' : 'Показать пароль'}
+          >
+            {isPasswordVisible ? <EyeSlash /> : <Eye />}
+          </button>
+        )}
         {error && <label className={styles['input__error-label']}>{errorText}</label>}
       </div>
     )
